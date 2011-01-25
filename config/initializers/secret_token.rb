467075b5653dd1385a438a5aa5d230f9a163547d
smileyfaced32 <fiore.dennis@gmail.com>
2011-01-25 09:08:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greatlakesmulti::Application.config.secret_token = 'dae913cefb4690f7b7ec836133cd0e52ec8948276f4b237f3e849790669f1280fce771ed53e6a7f51b360a345845568c426935c8f0d7fd13d64878da6bf340a2'
