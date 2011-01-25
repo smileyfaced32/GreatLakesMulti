08f955ee11d150d391b6211d412865499aebc7de
smileyfaced32 <fiore.dennis@gmail.com>
2011-01-25 09:29:54 -0500

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,26 +1,47 @@
-require 'spec_helper'
+ require 'spec_helper'
 
-describe PagesController do
+ describe PagesController do
+   render_views
 
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-  end
+   describe "GET 'home'" do
+     it "should be successful" do
+       get 'home'
+       response.should be_success
+     end
 
-  describe "GET 'contact'" do
-    it "should be successful" do
-      get 'contact'
-      response.should be_success
-    end
-  end
+     it "should have the right title" do
+       get 'home'
+       response.should have_selector("title",
+                         :content => "Great Lakes Multimedia | Home")
+     end
+   end
 
-  describe "GET 'about'" do
-    it "should be successful" do
-      get 'about'
-      response.should be_success
-    end
-  end
+   describe "GET 'contact'" do
+     it "should be successful" do
+       get 'contact'
+       response.should be_success
+     end
 
-end
+     it "should have the right title" do
+       get 'contact'
+       response.should have_selector("title",
+                         :content =>
+                           "Great Lakes Multimedia | Contact")
+     end
+   end
+
+   describe "GET 'about'" do
+     it "should be successful" do
+       get 'about'
+       response.should be_success 
+     end
+
+     it "should have the right title" do
+       get 'about'
+       response.should have_selector("title",
+                         :content => 
+                           "Great Lakes Multimedia | About")
+     end
+   end
+ end
+ 
